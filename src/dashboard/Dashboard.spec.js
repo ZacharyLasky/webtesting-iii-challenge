4fc63d70743b8cb392aa35ba14a82f022996ee9a
Zach Lasky <zacharylasky123@gmail.com>
2019-08-19 16:44:46 -0400

    added basic snapshots for each component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './dashboard';
+
+describe('<Dashboard />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
+
+// describe('asyncFunc', () => {
+//   it('eventually resolves to success', () => {
+//     const resolvedValue = null;
+//     asyncFunc().then(res => {
+//       resolvedValue = res
+//       expect(resolvedValue).toEqual(expected)
+//     })
+//   })
+// })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './display';
+
+describe('<Display />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Display />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
