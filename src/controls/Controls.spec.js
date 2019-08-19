c9ef3d051a57c926e4a838f0991dbe8c5b28bc00
Zach Lasky <zacharylasky123@gmail.com>
2019-08-19 18:56:18 -0400

    controls tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
 
 import Controls from './controls';
 
@@ -12,4 +13,28 @@ describe('<Controls />', () => {
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it('should button fire when toggle locked is called', () => {
+    const mockClosed = jest.fn()
+    const { getByText } = render(<Controls toggleLocked={mockClosed} locked={true} closed={true}/>)
+    fireEvent.click(getByText("Unlock Gate"))
+    expect(mockClosed).toHaveBeenCalled()
+  })
+
+  it('should button fire when toggle closed is called', () => {
+    const mockClosed = jest.fn()
+    const { getByText } = render(<Controls toggleClosed={mockClosed} locked={false} closed={false}/>)
+    fireEvent.click(getByText("Close Gate"))
+    expect(mockClosed).toHaveBeenCalled()
+  })
+
+  it("should", () => {
+    const { getByText } = render(<Controls/>)
+    expect(getByText("Lock Gate").disabled).toBeTruthy()
+  })
+  
+  it("should", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />)
+    expect(getByText("Open Gate").disabled).toBeTruthy()
+  })
 });
